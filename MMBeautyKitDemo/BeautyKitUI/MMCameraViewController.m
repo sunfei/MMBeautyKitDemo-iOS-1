97ecfe28e25fa186c9724735d92f03d4b8f5fd15
sunfei <sun.fei@immomo.com>
2020-09-02 12:21:55 +0800

    ss

--- MMBeautyKitDemo/BeautyKitUI/MMCameraViewController.m
+++ MMBeautyKitDemo/BeautyKitUI/MMCameraViewController.m
@@ -243,6 +243,7 @@
         @{@"name":@"嘴唇厚度",@"type":LIP_THICKNESS,@"begin":@-1, @"end":@1},
         @{@"name":@"嘴唇大小",@"type":MOUTH_SIZE,@"begin":@-1, @"end":@1},
         @{@"name":@"宽颔",@"type":JAWWIDTH, @"begin":@-1, @"end":@1},
+        
     ];
     
     NSMutableArray<MMSegmentItem *> *items = [NSMutableArray array];
