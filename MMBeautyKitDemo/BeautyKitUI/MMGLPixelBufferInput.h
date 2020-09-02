9c06de99c798f93eb0028cfb08e14f3fb78dcca5
sunfei <sun.fei@immomo.com>
2020-09-02 19:11:06 +0800

    [update] core image

---
+++ MMBeautyKitDemo/BeautyKitUI/MMGLPixelBufferInput.h
@@ -0,0 +1,17 @@
+//
+//  MMGLPixelBufferInput.h
+//  MMBeautyKitDemo
+//
+//  Created by sunfei on 2020/9/2.
+//  Copyright © 2020 sunfei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MMGLPixelBufferInput : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
